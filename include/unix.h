21235b65e577653dfb08232a31c6e8289e525f26
theoddone33 <theoddone33@icculus.org>
2002-05-26 20:07:13 +0000

    Committing my version

--- include/unix.h
+++ include/unix.h
@@ -11,6 +11,7 @@
 #define DWORD int
 #define _MAX_FNAME 255
 #define _MAX_PATH 255
+#define MAX_PATH 255
 #define _MAX_DIR 256
 #define MAX_FILENAME_LENGTH 64
 #define _cdecl
@@ -21,6 +22,7 @@
 #define _strnicmp strncasecmp
 #define _isnan isnan
 #define HANDLE int
+#define HINSTANCE int
 #define _getcwd getcwd
 #define _chdir chdir
 #define _strlwr strlwr
@@ -28,7 +30,8 @@
 #define _mkdir mkdir
 #define _hypot hypot
 #define byte unsigned char
-#define LPSTR char *
+#define __try try
+#define __except catch
 
 extern void strlwr (char *str);
 extern int filelength (int fd);
@@ -37,6 +40,14 @@ extern int MulDiv (int, int, int);
 extern void Sleep (int miliseconds);
 extern unsigned long _beginthread (void (*pfuncStart)(void *), unsigned unStackSize, void* pArgList);
 
+typedef struct FILETIME_s {
+	    DWORD dwLowDateTime;
+	    DWORD dwHighDateTime;
+} FILETIME, *PFILETIME;
+
+extern void strlwr (char *str);
+extern int filelength (int fd);
+extern int MulDiv (int, int, int);
 #define CRITICAL_SECTION SDL_mutex*
 
 #define STUB_FUNCTION fprintf(stderr,"STUB: %s at " __FILE__ ", line %d, thread %d\n",__FUNCTION__,__LINE__,getpid())

409837d8b724108ae98bf1822f9f234195e1f802
relnev <relnev@icculus.org>
2002-05-27 23:43:54 +0000

    call real entrypoint (WinMain)

--- src/freespace2/unixmain.cpp
+++ src/freespace2/unixmain.cpp
@@ -1,6 +1,10 @@
 #include "unix.h"
 
+int PASCAL WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR szCmdLine, int nCmdShow);
+
 int main(int argc, char **argv)
 {
 	STUB_FUNCTION;
+
+	return WinMain(1, 0, "", 0);
 }

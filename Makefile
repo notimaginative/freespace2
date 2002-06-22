6ec95cb60a02cf99ab6a36577a024b7f62e41d61
relnev <relnev@icculus.org>
2002-06-22 16:34:40 +0000

    removed file

--- Makefile
+++ Makefile
@@ -157,7 +157,6 @@ CODE_SOURCES =./src/anim/animplay.cpp \
 	./src/parse/sexp.cpp \
 	./src/sound/rtvoice.cpp \
 	./src/sound/sound.cpp \
-	./src/sound/acm-unix.cpp \
 	./src/sound/audiostr.cpp \
 	./src/sound/ds.cpp \
 	./src/vcodec/codec1.cpp \
--- src/sound/sound.cpp
+++ src/sound/sound.cpp
@@ -15,6 +15,9 @@
  * Low-level sound code
  *
  * $Log$
+ * Revision 1.9  2002/06/22 16:34:40  relnev
+ * removed file
+ *
  * Revision 1.8  2002/06/17 06:33:11  relnev
  * ryan's struct patch for gcc 2.95
  *
@@ -454,16 +457,14 @@ int snd_init(int use_a3d, int use_eax)
 		}
 	}
 
+#ifndef PLAT_UNIX
 	// Init the Audio Compression Manager
 	if ( ACM_init() == -1 ) {
-#ifndef PLAT_UNIX
 		HWND hwnd = (HWND)os_get_window();
 		MessageBox(hwnd, XSTR("Could not properly initialize the Microsoft ADPCM codec.\n\nPlease see the readme.txt file for detailed instructions on installing the Microsoft ADPCM codec.",972), NULL, MB_OK);
-#else
-		fprintf (stderr, "ACM_init failed\n");
-#endif
 //		Warning(LOCATION, "Could not properly initialize the Microsoft ADPCM codec.\nPlease see the readme.txt file for detailed instructions on installing the Microsoft ADPCM codec.");
 	}
+#endif
 
 	// Init the audio streaming stuff
 	audiostream_init();
@@ -716,7 +717,9 @@ void snd_close(void)
 	snd_stop_all();
 	if (!ds_initialized) return;
 	snd_unload_all();		// free the sound data stored in DirectSound secondary buffers
+#ifndef PLAT_UNIX
 	ACM_close();	// Close the Audio Compression Manager (ACM)
+#endif	
 	ds3d_close();	// Close DirectSound3D
 	dscap_close();	// Close DirectSoundCapture
 	ds_close();		// Close DirectSound off

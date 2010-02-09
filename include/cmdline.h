ad3b537f606279b896cdfe1d45bf9fddd06c6081
Taylor Richards <taylor@icculus.org>
2010-02-09 21:00:38 +0000

    missing valgrind warning fix from previous commit

--- include/cmdline.h
+++ include/cmdline.h
@@ -156,7 +156,7 @@
 #ifndef FS_CMDLINE_HEADER_FILE
 #define FS_CMDLINE_HEADER_FILE
 
-int parse_cmdline(char *cmdline);
+int parse_cmdline(char *cmdline = NULL);
 
 // COMMAND LINE SETTINGS
 // This section is for reference by all the *_init() functions. For example, the multiplayer init function

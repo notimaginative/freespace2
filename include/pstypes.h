7428688b1ad1b4d2d9812ba658150fb602f60cdc
Taylor Richards <taylor@icculus.org>
2014-11-06 16:01:12 -0500

    bump MAX_PATH_LEN to 256

--- include/pstypes.h
+++ include/pstypes.h
@@ -536,7 +536,7 @@ inline int mul_div(int n, int num, int dem)
 
 // Some constants for stuff
 #define MAX_FILENAME_LEN	32			// Length for filenames, ie "title.pcx"
-#define MAX_PATH_LEN			128		// Length for pathnames, ie "c:\bitmaps\title.pcx"
+#define MAX_PATH_LEN		256			// Length for pathnames, ie "c:\bitmaps\title.pcx"
 
 // contants and defined for byteswapping routines (useful for mac)
 

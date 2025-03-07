b517468cb1eceeb018508332a3075e376a200278
Taylor Richards <taylor@notimaginative.com>
2025-03-07 12:12:37 -0500

    fix goal bug in previous goal bug fix

--- src/ship/aigoals.cpp
+++ src/ship/aigoals.cpp
@@ -1838,19 +1838,16 @@ int ai_goal_priority_compare(const void *a, const void *b)
 		return -1;
 	else if ( ga->priority < gb->priority )
 		return 1;
-	else {
-		if ( ga->time > gb->time )
-			return -1;
-#ifdef PLAT_UNIX
-		else if ( ga->time > gb->time )
-			return 1;
-		else
-			return 0;
-#else
-		else // if ( ga->time < gb->time )			// this way prevents element swapping if times happen to be equal (which they should not)
-			return 1;
-#endif
-	}
+
+	// now check time
+
+	if ( ga->time > gb->time )
+		return -1;
+	else if ( ga->time < gb->time )
+		return 1;
+
+	// the two are equal
+	return 0;
 }
 
 //	Prioritize goal list.

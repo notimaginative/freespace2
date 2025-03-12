2d75530b8387e2eaa2e4ecfc808fd60663fe1bea
Taylor Richards <taylor@notimaginative.com>
2025-03-11 20:18:57 -0400

    create 2x version of mouse cursor for use with high dpi displays

--- src/io/mouse.cpp
+++ src/io/mouse.cpp
@@ -187,22 +187,42 @@ SDL_Cursor *mouse_create_cursor(int bmap_id)
 		return nullptr;
 	}
 
+	SDL_Surface *surface = nullptr;
 	auto bmp = bm_lock(bmap_id, 16, BMP_TEX_XPARENT);
 
 	if ( !bmp ) {
 		return nullptr;
 	}
 
-	auto surface = SDL_CreateSurfaceFrom(bmp->w, bmp->h,
-										 SDL_PIXELFORMAT_RGBA5551,
-										 reinterpret_cast<void *>(bmp->data),
-										 bmp->rowsize * 2);
+	auto w_2 = bmp->w * 2;
+	auto h_2 = bmp->h * 2;
+
+	auto surfaceOrig = SDL_CreateSurfaceFrom(bmp->w, bmp->h,
+											 SDL_PIXELFORMAT_RGBA5551,
+											 reinterpret_cast<void *>(bmp->data),
+											 bmp->rowsize * 2);
+
+	// convert to 32-bit
+	if (surfaceOrig) {
+		surface = SDL_ConvertSurface(surfaceOrig, SDL_PIXELFORMAT_ARGB8888);
+		SDL_DestroySurface(surfaceOrig);
+		surfaceOrig = nullptr;
+	}
 
 	bm_unlock(bmap_id);
 	bmp = nullptr;
 
 	if ( !surface ) {
 		return nullptr;
+	}
+
+	// create alternate x2 version for hi-dpi
+	auto surfaceScaled = SDL_ScaleSurface(surface, w_2, h_2, SDL_SCALEMODE_LINEAR);
+
+	if (surfaceScaled) {
+		SDL_AddSurfaceAlternateImage(surface, surfaceScaled);
+		SDL_DestroySurface(surfaceScaled);
+		surfaceScaled = nullptr;
 	}
 
 	auto cursor = SDL_CreateColorCursor(surface, 0, 0);

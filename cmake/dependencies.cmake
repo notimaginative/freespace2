8c66d29de1662a5940e0b966f31116b5b81e1f32
Taylor Richards <taylor@notimaginative.com>
2025-09-20 23:51:31 -0400

    bump libwebsockets version and fix msvc warnings

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,14 +1,11 @@
 include(FetchContent)
 
-# save current compiler flags and disable warnings
-set(C_FLAGS_save ${CMAKE_C_FLAGS})
-set(CXX_FLAGS_save ${CMAKE_CXX_FLAGS})
-set(SHARED_LINKER_FLAGS_save ${CMAKE_SHARED_LINKER_FLAGS})
+# save current compiler flags and disable warnings (gcc/clang)
+if(NOT MSVC)
+  set(C_FLAGS_save ${CMAKE_C_FLAGS})
+  set(CXX_FLAGS_save ${CMAKE_CXX_FLAGS})
+  set(SHARED_LINKER_FLAGS_save ${CMAKE_SHARED_LINKER_FLAGS})
 
-if(MSVC)
-  set(CMAKE_C_FLAGS "/W0")
-  set(CMAKE_CXX_FLAGS "/W0")
-else()
   set(CMAKE_C_FLAGS "-w")
   set(CMAKE_CXX_FLAGS "-w")
   set(CMAKE_SHARED_LINKER_FLAGS "-w")
@@ -21,13 +18,10 @@ endif()
 
 FetchContent_Declare(
   SDL3
-  #GIT_REPOSITORY https://github.com/libsdl-org/SDL
-  #GIT_TAG main
-  #GIT_SHALLOW ON
   URL https://github.com/libsdl-org/SDL/releases/download/release-3.2.20/SDL3-3.2.20.zip
+  DOWNLOAD_EXTRACT_TIMESTAMP OFF
   EXCLUDE_FROM_ALL
   SYSTEM
-  DOWNLOAD_EXTRACT_TIMESTAMP OFF
 )
 
 FetchContent_MakeAvailable(SDL3)
@@ -38,13 +32,10 @@ FetchContent_MakeAvailable(SDL3)
 
 FetchContent_Declare(
   OpenAL
-  #GIT_REPOSITORY https://github.com/kcat/openal-soft
-  #GIT_TAG master
-  #GIT_SHALLOW ON
   URL https://github.com/kcat/openal-soft/archive/refs/tags/1.24.3.zip
+  DOWNLOAD_EXTRACT_TIMESTAMP OFF
   EXCLUDE_FROM_ALL
   SYSTEM
-  DOWNLOAD_EXTRACT_TIMESTAMP OFF
 )
 
 set(ALSOFT_UTILS OFF CACHE BOOL "")
@@ -67,19 +58,12 @@ if(NOT EMSCRIPTEN)
   # libwebsockets
   #
 
-  # old cmake file in project but policy 3.10 appears to work just fine
-  set(MIN_POLICY_save ${CMAKE_POLICY_VERSION_MINIMUM})
-  set(CMAKE_POLICY_VERSION_MINIMUM "3.10")
-
   FetchContent_Declare(
     libwebsockets
-    #GIT_REPOSITORY https://github.com/warmcat/libwebsockets
-    #GIT_TAG main
-    #GIT_SHALLOW ON
-    URL https://github.com/warmcat/libwebsockets/archive/refs/tags/v4.3.5.zip
+    URL https://github.com/warmcat/libwebsockets/archive/refs/tags/v4.4.1.zip
+    DOWNLOAD_EXTRACT_TIMESTAMP OFF
     EXCLUDE_FROM_ALL
     SYSTEM
-    DOWNLOAD_EXTRACT_TIMESTAMP OFF
   )
 
   set(LWS_WITH_SSL OFF CACHE BOOL "")
@@ -87,11 +71,12 @@ if(NOT EMSCRIPTEN)
 
   FetchContent_MakeAvailable(LibWebSockets)
 
-  set(CMAKE_POLICY_VERSION_MINIMUM ${MIN_POLICY_save})
 endif()
 
 
-# reset compiler flags
-set(CMAKE_C_FLAGS ${C_FLAGS_save})
-set(CMAKE_CXX_FLAGS ${CXX_FLAGS_save})
-set(CMAKE_SHARED_LINKER_FLAGS ${SHARED_LINKER_FLAGS_save})
+# reset compiler flags (gcc/clang)
+if(NOT MSVC)
+  set(CMAKE_C_FLAGS ${C_FLAGS_save})
+  set(CMAKE_CXX_FLAGS ${CXX_FLAGS_save})
+  set(CMAKE_SHARED_LINKER_FLAGS ${SHARED_LINKER_FLAGS_save})
+endif()

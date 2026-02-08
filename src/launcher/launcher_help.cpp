ab15da2ae137f2c4e10a53d8bb9674813e51a5b5
Taylor Richards <taylor@notimaginative.com>
2026-02-07 23:36:56 -0500

    fix launcher font size issues

--- src/launcher/launcher_help.cpp
+++ src/launcher/launcher_help.cpp
@@ -258,7 +258,7 @@ void launcher_help_draw()
 
 	ImGui::Begin("Help", nullptr, window_flags);
 
-	ImGui::PushFont(Fonts[FONT_MONO].ptr, 0.0f);
+	ImGui::PushFont(Fonts[FONT_MONO].ptr, WindowScale->get(Fonts[FONT_MONO].size));
 	ImGui::SetNextWindowSize(ImVec2(0.f, i2fl(h) - WindowScale->get(75.f)));
 	ImGui::BeginChild("text");
 	ImGui::TextUnformatted(HelpText ? HelpText : "No help available.");
